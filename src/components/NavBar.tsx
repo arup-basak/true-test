c5a192babf0fd00b10c4fd70380519856b33dd22
Arup Basak <95271566+arup-basak@users.noreply.github.com>
2023-06-15 00:01:04 +0530

    Added Navbar links

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import Link from 'next/link'
 
 const NavBar = () => {
     return (
-        <div className='grid grid-cols-3'>
-            <h1>True Test</h1>
+        <div className='grid grid-cols-4'>
+            <Link href="/">
+                <h1 className='text-2xl'>True Test</h1>
+            </Link>
             <div className='flex justify-between w-4/5'>
                 <div>Link 1</div>
                 <div>Link 2</div>
@@ -11,6 +14,9 @@ const NavBar = () => {
                 <div>Link 4</div>
             </div>
             <button className='p-2 px-4 bg-purple-300 rounded w-fit'>Call Now</button>
+            <Link href="/cart">
+                <button className='p-2 px-4 bg-purple-300 rounded w-fit'>Cart</button>
+            </Link>
         </div>
     )
 }
