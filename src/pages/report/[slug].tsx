d6dff09b0de8b8b335ce1ed36f6bbe15d2a9d68b
Arup Basak <95271566+arup-basak@users.noreply.github.com>
2023-06-20 22:33:11 +0530

    fetching slug

--- src/pages/report/[slug].tsx
+++ src/pages/report/[slug].tsx
@@ -1,20 +1,64 @@
-import Head from 'next/head'
-import { useRouter } from 'next/router'
+import Head from 'next/head';
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
+import { jsPDF } from "jspdf";
 
 const Post = () => {
-    const router = useRouter()
-    const { slug } = router.query
+    const router = useRouter();
+    const { slug } = router.query;
+    const [data, setData] = useState("");
+    const [loading, setLoading] = useState(true);
+
+    const handleGeneratePdfClick = () =>  {
+        
+    }
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const url = `http://localhost:3000/api/reports?id=${slug}`;
+                console.log(url)
+                const response = await fetch(url);
+                const jsonData = await response.json();
+                setData(jsonData['patientName']);
+                setLoading(false);
+            } catch (error) {
+                console.error('Error fetching data:', error);
+                setLoading(false);
+            }
+        };
+
+        if (slug) {
+            fetchData();
+        }
+    }, [slug]);
+
+    if (loading) {
+        return (
+            <>
+                <Head>
+                    <title>Loading...</title>
+                </Head>
+                <div>
+                    Loading...
+                </div>
+            </>
+        );
+    }
+
+
 
     return (
         <main>
             <Head>
-                <title>{slug}</title>
+                <title>{data}</title>
             </Head>
             <div>
-                post: {slug}
+                {data}
+                <button className="bg-red-800 p-2" onClick={handleGeneratePdfClick}>Generate PDF</button>
             </div>
         </main>
-    )
-}
+    );
+};
 
 export default Post;
