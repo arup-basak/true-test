2c6ca41c93cc56c909996471d8d60b6e90dc4c30
Arup Basak <95271566+arup-basak@users.noreply.github.com>
2023-06-23 12:14:16 +0530

    updated sitemap

--- src/pages/sitemap.xml.tsx
+++ src/pages/sitemap.xml.tsx
@@ -2,35 +2,43 @@ import { GetServerSideProps } from 'next';
 
 function generateSiteMap() {
     return `<?xml version="1.0" encoding="UTF-8"?>
+    <?xml version="1.0" encoding="UTF-8"?>
     <urlset
           xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
           xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
           xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9
                 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
+    
+    
     <url>
       <loc>https://true-test.vercel.app/</loc>
-      <lastmod>2023-06-15T11:51:32+00:00</lastmod>
+      <lastmod>2023-06-23T06:42:51+00:00</lastmod>
       <priority>0.80</priority>
     </url>
     <url>
-      <loc>https://true-test.vercel.app/labs</loc>
-      <lastmod>2023-06-15T11:51:32+00:00</lastmod>
-      <priority>0.90</priority>
+      <loc>https://true-test.vercel.app/cart</loc>
+      <lastmod>2023-06-23T06:42:51+00:00</lastmod>
+      <priority>0.30</priority>
     </url>
     <url>
-      <loc>https://true-test.vercel.app/products</loc>
-      <lastmod>2023-06-15T11:51:32+00:00</lastmod>
-      <priority>1</priority>
+      <loc>https://true-test.vercel.app/labs</loc>
+      <lastmod>2023-06-23T06:42:51+00:00</lastmod>
+      <priority>0.90</priority>
     </url>
     <url>
       <loc>https://true-test.vercel.app/about</loc>
-      <lastmod>2023-06-15T11:51:32+00:00</lastmod>
-      <priority>0.70</priority>
+      <lastmod>2023-06-23T06:42:51+00:00</lastmod>
+      <priority>0.80</priority>
     </url>
     <url>
-      <loc>https://true-test.vercel.app/cart</loc>
-      <lastmod>2023-06-15T11:51:32+00:00</lastmod>
-      <priority>0.90</priority>
+      <loc>https://true-test.vercel.app/report</loc>
+      <lastmod>2023-06-23T06:42:51+00:00</lastmod>
+      <priority>1.00</priority>
+    </url>
+    <url>
+      <loc>https://true-test.vercel.app/products</loc>
+      <lastmod>2023-06-23T06:42:51+00:00</lastmod>
+      <priority>0.30</priority>
     </url>
     
     
