61bb68c5346e2165b1e2e7e7a8c7f0a2dd5c1399
Arup Basak <95271566+arup-basak@users.noreply.github.com>
2023-06-29 17:46:13 +0530

    optimized file structure

--- src/pages/505.tsx
+++ src/pages/505.tsx
@@ -1,13 +1,13 @@
 import { motion } from 'framer-motion';
 import Head from 'next/head';
 import Image from 'next/image';
-import icon_404 from 'public/icon-404.svg';
+import icon_500 from 'public/icon-500.svg';
 
-export default function Custom404() {
+export default function Custom500() {
   return (
     <>
       <Head>
-        <title>404 Not Found</title>
+        <title>500 Internal Server Error</title>
       </Head>
       <div className="min-h-screen bg-gray-100 flex justify-center items-center">
         <motion.div
@@ -16,11 +16,11 @@ export default function Custom404() {
           exit={{ opacity: 0, y: 50 }}
           className="flex flex-col items-center space-y-6"
         >
-          <Image src={icon_404} alt="404icon" height={100} width={100} />
-          <div className="text-6xl font-bold text-transparent bg-gradient-to-r from-blue-500 to-green-400 bg-clip-text">
-            Oh! Snap
+          <Image src={icon_500} alt="500icon" height={100} width={100} />
+          <div className="text-6xl font-bold text-transparent bg-gradient-to-r from-red-500 to-yellow-400 bg-clip-text">
+            Oops!
           </div>
-          <div className="text-3xl">Page Not Found</div>
+          <div className="text-3xl">Internal Server Error</div>
         </motion.div>
       </div>
     </>
